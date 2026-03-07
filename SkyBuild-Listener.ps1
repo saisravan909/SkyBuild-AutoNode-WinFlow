2ac2ef1c6908c16c18d45bebdb92248c6e870ff3
Sai Sravan Cherukuri <saisravan@gmail.com>
2026-03-07 11:51:38 -0500

    Update SkyBuild-Listener.ps1

--- SkyBuild-Listener.ps1
+++ SkyBuild-Listener.ps1
@@ -19,7 +19,7 @@ try {
             Write-Host "Signal received! Triggering Invoke-WinFlow..." -ForegroundColor Cyan
             
             # This calls the engine in your scripts folder
-            .\scripts\Invoke-WinFlow.ps1 -JsonPayload $payload
+            .\scripts\Invoke-WinFlow.ps1 -JsonPayload ($payload | Out-String)
         }
 
         $response = $context.Response
